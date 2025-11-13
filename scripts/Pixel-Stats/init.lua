66aa5b642feee7c7bcff7f095158b0ebd1300b56
zhangsan01 <487879313@qq.com>
2025-11-13 22:55:15 +0800

    新增画板像素统计插件，待翻译

---
+++ scripts/Pixel-Stats/init.lua
@@ -0,0 +1,18 @@
+local Pixel_Stats_I18n = I18n.new()
+Pixel_Stats_I18n:init("scripts.Pixel_Stats.locale")
+function init(plugin)
+  plugin:newCommand {
+    id = "pixel-status",
+    title = "pixel-status",
+    group = "layer_merge",
+    onclick = function()
+      loadfile(Global_Config.SYS_EXTENSIONS_DIR("Pixel-Stats", "pixel_stats.lua"))()
+    end,
+    onenabled = function()
+      return not not app.sprite
+    end
+  }
+end
+
+function exit(plugin)
+end
---
+++ scripts/Pixel-Stats/locale/zh.lua
@@ -0,0 +1,17 @@
+return {
+  msg_title = "Image Statistics",
+  selected_text1 = "Selection size:",
+  selected_text2 = "Image size:",
+  total_text1 = "Total amount of pixels:",
+  total_text2 = "Total amount of colors:",
+
+  e_title = "ERROR",
+  e_text1 = "The amount of colors in this image went over the limit",
+  e_text2 = "of",
+  e_text3 = "colors so the script stopped so Aseprite won't crash.",
+  e_text4 = "If you want to change the max amount of colors to",
+  e_text5 = "anaylze, you can change thevalue of 'maxColors'",
+  e_text6 = "in the pixel_stats.lua script.",
+
+  e_text7 = "Grouped Color Statistics",
+}
